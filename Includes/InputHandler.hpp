91675fd59a59b18dfe6b0d46bc1123318eafe682
Victor-Hamon <51273603+Victor-Hamon@users.noreply.github.com>
2021-08-13 20:23:23 +0200

    Push

--- Includes/InputHandler.hpp
+++ Includes/InputHandler.hpp
@@ -5,8 +5,34 @@
 #ifndef INC_2048_INPUTHANDLER_HPP
 #define INC_2048_INPUTHANDLER_HPP
 
-class InputHandler {
-    
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <SFML/Window/Event.hpp>
+#include <map>
+
+class Board;
+
+struct KeyboardInput {
+    std::function<int(std::shared_ptr<Board>)> _callback;
+    sf::Keyboard::Key _key;
 };
+
+namespace InputCallbacks {
+    int InputFunction_Move_UP(std::shared_ptr<Board> boardInstance);
+}
+
+class InputHandler {
+    private:
+    std::map<sf::Keyboard::Key, std::pair<std::function<int(std::shared_ptr<Board>)>, const std::string>> _inputs = {
+            {sf::Keyboard::Key::Up, std::make_pair(InputCallbacks::InputFunction_Move_UP, "Move_UP")},
+            {sf::Keyboard::Key::Down, std::make_pair(InputCallbacks::InputFunction_Move_UP, "Move_UP")},
+            {sf::Keyboard::Key::Left, std::make_pair(InputCallbacks::InputFunction_Move_UP, "Move_UP")},
+            {sf::Keyboard::Key::Right, std::make_pair(InputCallbacks::InputFunction_Move_UP, "Move_UP")}
+    };
+
+public:
+};
+
 
 #endif //INC_2048_INPUTHANDLER_HPP
